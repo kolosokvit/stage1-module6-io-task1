73015105261caa01bb3face64a8e8af6a2e3e812
Vitali Kalaskou <kolosokvit@gmail.com>
2023-08-08 23:10:15 +0300

    Complete task

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,24 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            StringBuilder data = new StringBuilder();
+            while (fileInputStream.available() > 0) {
+                data.append((char) fileInputStream.read());
+            }
+            String[] personalData = data.toString().split(System.lineSeparator());
+            String name = personalData[0].split(": ")[1];
+            int age = Integer.parseInt(personalData[1].split(": ")[1]);
+            String email = personalData[2].split(": ")[1];
+            long phone = Long.parseLong(personalData[3].split(": ")[1]);
+            return new Profile(name, age, email, phone);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
